10b6ecc0cb785532e58011f982773bd0f6320f70
Socrates Vlassis <socrates.vlassis@gmail.com>
2024-02-04 22:24:15 +0900

    Update build_exe.bat

--- build_exe.bat
+++ build_exe.bat
@@ -1,5 +1,6 @@
-pyinstaller src\akrocansim.py ^
+pyinstaller src\akrocansim_main.py ^
             --onefile --noconsole ^
             --add-data="src/akrocansim/resources/akrocansim.ico:akrocansim/resources" ^
             --add-data="src/akrocansim/resources/akrocansim_logo_dark.png:akrocansim/resources" ^
-            --icon src/akrocansim/resources/akrocansim.ico
+            --icon src/akrocansim/resources/akrocansim.ico ^
+            --name akrocansim
