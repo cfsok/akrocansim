8e9cc5738f109e06f02626906537fa0c05ec4d26
Philosokos <33849525+Philosokos@users.noreply.github.com>
2026-06-12 11:43:37 +0900

    Bumped python-can and dearpygui versions

--- build_exe.bat
+++
@@ -1,6 +0,0 @@
-pyinstaller src\akrocansim_main.py ^
-            --onefile --noconsole ^
-            --add-data="src/akrocansim/resources/akrocansim.ico:akrocansim/resources" ^
-            --add-data="src/akrocansim/resources/akrocansim_logo_dark.png:akrocansim/resources" ^
-            --icon src/akrocansim/resources/akrocansim.ico ^
-            --name akrocansim
